d7e54d0c41b07c4865b1120486c0ad0d61c69165
Kenneth Thompson <ken.thompson23@gmail.com>
2017-02-26 23:45:24 -0500

    Move to base template. Basic image functionality.
    - switch to flask_script due to deprecation warning.
    - create basic 404 and home page.

--- weddingPictures/index.py
+++ weddingPictures/index.py
@@ -1,21 +1,39 @@
-from flask import Flask, render_template
-from flask.ext.script import Manager
+from flask import Flask, render_template, redirect, abort
+from flask_script import Manager
+import os
 app = Flask(__name__)
 manager = Manager(app)
+
 
 @app.route('/')
 def index():
     '''
-    Redirect the user to the wedding page
+    Default landing page
     '''
-    return 'Redirect to wedding.'
+    return render_template('home.html')
 
 @app.route('/photos/<event>')
 def photos(event):
     '''
     Display a photos for a passed in event.
     '''
-    return render_template('event.html', event=event)
+
+    available_events = ['wedding', 'engagement']
+
+    if event not in available_events:
+        abort(404)
+
+
+    images = []
+    image_path = os.path.join('weddingPictures', 'static', 'images', event, 'small')
+    for image in os.scandir(image_path):
+        images.append(image.name)
+
+    return render_template('event.html', event=event, images=images)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     manager.run()
