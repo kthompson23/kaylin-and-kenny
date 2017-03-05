ebd5a568532ee83ab794853977d6e76fef557355
Kenneth Thompson <ken.thompson23@gmail.com>
2017-03-04 22:09:12 -0500

    Move events route to main views.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, abort
 from . import main
+
+from ..database.data_access import DataAccess
 
 @main.route('/', methods=['GET'])
 def index():
@@ -7,3 +9,19 @@ def index():
     Default landing page
     '''
     return render_template('home.html')
+
+@main.route('/photos/<event>')
+def photos(event):
+    '''
+    Display a photos for a passed in event.
+    '''
+
+    available_events = ['wedding', 'engagement']
+
+    if event not in available_events:
+        abort(404)
+
+    with DataAccess() as db:
+        images = db.get_images(event)
+
+    return render_template('event.html', event=event, images=images)
