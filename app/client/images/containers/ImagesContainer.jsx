00e6423c50df7d49772ff2802ce818753e8c09db
Kenneth Thompson <ken.thompson23@gmail.com>
2017-03-19 17:37:36 -0400

    Add prestart clean scripts to npm start script.

--- app/client/images/containers/ImagesContainer.jsx
+++ app/client/images/containers/ImagesContainer.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
-const ImagesContainer = () => {
-    return(
-        <div>
-            Sup?
-        </div>
+class ImagesContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      page: 0,
+      totalImages: 0,
+      totalPages: 0,
+      isFetching: 0,
+      images: [],
+    };
+  }
+
+  componentDidMount() {
+    const event = 'wedding';
+    const page = 0;
+    loadImages(event, page);
+  }
+
+  loadImages(event, page) {
+
+  }
+
+  render() {
+    return (
+      <div>
+        Sup baby?
+</div>
     )
-};
+  };
+}
 
 export default ImagesContainer;
